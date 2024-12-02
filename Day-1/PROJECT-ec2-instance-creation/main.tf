985be36f91425352dac68e2a213df97148748be7
Siddesh P M <109099693+siddeshpm1@users.noreply.github.com>
2024-12-02 12:33:57 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "us-east-1"  # Set your desired AWS region
-    access_key = "AKIA2UC3BCCGTFPKPJDR"     
-    secret_key = "82OUTac2naKLv4cv47PCAMJQElchHaP3U2QB7n+O"
+    access_key = "AKIA2UC3BCCG7E4I3XV6"     
+    secret_key = "xYJNJkN1jK8qHgLuN12Jt0hQ1jnzvbfLzHDrGWGl"
 }
 
 resource "aws_instance" "example" {
