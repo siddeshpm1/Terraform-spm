dc59208dcbbb11ee094451a6131095e4dc2be2f8
Siddesh P M <109099693+siddeshpm1@users.noreply.github.com>
2024-12-02 12:19:38 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,10 @@
 provider "aws" {
     region = "us-east-1"  # Set your desired AWS region
+    access_key = "AKIA2UC3BCCGTFPKPJDR"     
+    secret_key = "82OUTac2naKLv4cv47PCAMJQElchHaP3U2QB7n+O"
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-0ed83e7a78a23014e"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
